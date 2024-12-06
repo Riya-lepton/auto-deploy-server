7bfb4a656569a3241ab029680a489e741506e420
Riya-lepton <riya.singh@leptonmaps.com>
2024-12-06 12:24:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Downloading artifact..."
-
+echo "Current user: $(whoami)"
+echo "Current directory: $(pwd)"
 gh run download --repo Riya-lepton/auto-deploy-server -n build-artifact --dir /home/lepton/riya_space/test
+echo "Artifact download complete"
